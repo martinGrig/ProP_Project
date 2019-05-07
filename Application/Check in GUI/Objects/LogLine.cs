2a816d28edba996c0dff36ddfafcbbc7594d5ee0
Hooi,David D.M <d.hooi@student.fontys.nl>
2019-05-07 18:32:22 +0200

    Added regex to Converter viewmodel to check the contents of loaded file and can load a visitor from the database with a rfid code

--- Application/Check in GUI/Objects/LogLine.cs
+++ Application/Check in GUI/Objects/LogLine.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace EventManager.Objects
 {
-    public class LogLine
+    public class LogLine : ObservableObject
     {
         public string Line { get; private set; }
         public string Type { get; private set; }
+        private bool _isEnabled;
+        public bool IsEnabled
+        {
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                _isEnabled = value;
+                OnPropertyChanged("IsEnabled");
+            }
+        }
 
         public LogLine(string line, string type)
         {
--- Application/Check in GUI/ViewModels/CampingViewModel.cs
+++ Application/Check in GUI/ViewModels/CampingViewModel.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Phidget22;
+using Phidget22.Events;
 
 namespace EventManager.ViewModels
 {
     public class CampingViewModel : IPageViewModel
     {
+        private RFID myRFIDReader;
+
+        public MainViewModel _mainViewModel { get; set; }
+        public CampingViewModel(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+            try
+            {
+                myRFIDReader = new RFID();
+                myRFIDReader.Tag += new RFIDTagEventHandler(GetCampingSpot);
+                myRFIDReader.Open();
+            }
+            catch (PhidgetException) { }
+        }
+        private void GetCampingSpot(object sender, RFIDTagEventArgs e)
+        {
+            _mainViewModel.dataModel.SelectedCampingSpot = _mainViewModel.dataHelper.GetCampingSpotByRFID(e.Tag);
+            if(_mainViewModel.dataModel.SelectedCampingSpot != null)
+            {
+                System.Windows.Forms.MessageBox.Show((_mainViewModel.dataModel.SelectedCampingSpot.AmountOfParticpants*20+20).ToString());
+            }
+        }
+
+
     }
 }
