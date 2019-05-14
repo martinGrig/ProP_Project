21f7a57a5872706f17d2d3cfedcaf8d3b7fe9edb
Мария Принц <m.khovanskaya@student.fontys.nl>
2019-05-14 16:47:25 +0200

    Anonym reviews added

--- Website/ReviewsProcess.php
+++ Website/ReviewsProcess.php
@@ -8,6 +8,10 @@
   $name = $_SESSION['currentUser']['name'];
   $isAnonym = $_POST['anonymous_checkbox'];
 
+  if($isAnonym == "anonymous") {
+    $name = "Anonymous";
+  }
+
   $con = new PDO('mysql:host=studmysql01.fhict.local;dbname=dbi410102', $username, $password);
 
     $sql = "INSERT INTO review (name, reviewText) VALUES (?, ?)";
